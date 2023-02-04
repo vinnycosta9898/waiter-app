ac0dda6e11346add4e13702a3b8d0d16dcc9aa81
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-02-04 13:48:18 -0300

    Criando a funcao AppRoutes para rotas privadas

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,1 +1,14 @@
-// Página de usuarios logados=
+// Página de usuarios logados
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { DashBoard } from "../Pages/DashBoard";
+
+const Stack = createNativeStackNavigator();
+
+export function AppRoutes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="DashBoard" component={DashBoard}/>
+        </Stack.Navigator>
+    )
+}
