edaf7434482a7c50e94c62f58655b76435cfb9a6
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-02-05 12:19:47 -0300

    Criando o componente Loading

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,10 +1,25 @@
-import { View } from "react-native";
+import { View, ActivityIndicator} from "react-native";
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 
 export function Routes(){
     const isAuthenticated = false;
-    const loading = false
+    const loading = true;
+
+    if(loading){
+        return(
+            <View 
+                style={{  
+                        flex: 1, 
+                        backgroundColor: "#F5F7FB",
+                        justifyContent: "center", 
+                        alignItems: "center"
+                        }}
+                        >
+                        <ActivityIndicator size={60} color="#1D1D2E"/>
+            </View>
+        )
+    }
     return(
         isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
     )
