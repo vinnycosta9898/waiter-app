8e5e852dfad50a48d91fc2216ade89e8f3291cd2
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-02-16 12:19:13 -0300

    Passando parametros para a Tela Order

--- mobile/src/Pages/Order/index.tsx
+++ mobile/src/Pages/Order/index.tsx
@@ -1,9 +1,20 @@
 import { View, Text, StyleSheet } from "react-native";
+import { useRoute, RouteProp} from '@react-navigation/native'
+
+type RouteDetailParams = {
+    Order: {
+        number: string | number;
+        order_id: string;
+    }
+}
+
+type OrderRouteProps = RouteProp<RouteDetailParams, "Order">;
 
 export function Order(){
+    const route = useRoute<OrderRouteProps>();
     return(
         <View style={styles.container}>
-            <Text>Order</Text>
+            <Text>{route.params.number}</Text>
         </View>
     )
 }
